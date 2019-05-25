41eddaa53b31005e89668d6a24af8359f2d0ea8a
Organela <andre.luiz.cunha95@gmail.com>
2019-05-25 17:11:34 -0300

    Versão Safe
    Sucesso

--- TrabPOO-Av2/src/Cadastro.java
+++ TrabPOO-Av2/src/Cadastro.java
@@ -1,7 +1,7 @@
-
+import java.sql.*;
 public class Cadastro {
-
-	public static void main(String[] args) {
+	static Connection conexao;
+	public static void main(String[] args)throws Exception {
 		// TODO Auto-generated method stub
 		
 		Cliente a = new Cliente("Daniel", 11147744, 999998888);
@@ -11,7 +11,56 @@ public class Cadastro {
 		a.Status(a);
 		b.Status(b);
 		c.Status(c);
+		
+		conecta();
+		System.out.println("Antes de executar");
 	
+		executa("INSERT INTO Cliente(nome, cpf, telefone) VALUES (" + a.getNome() + "," + a.getCpf() + "," + a.getTelefone() + ")");
+		executa("INSERT INTO Cliente(nome, cpf, telefone) VALUES (" + b.getNome() + "," + b.getCpf() + "," + b.getTelefone() + ")");
+		executa("INSERT INTO Cliente(nome, cpf, telefone) VALUES (" + c.getNome() + "," + c.getCpf() + "," + c.getTelefone() + ")");
+	
+		
+		System.out.println("Antes de listar Cliente");
+		listar_cliente();
+		executa("UPDATE Cliente SET nome = 'Joao' WHERE cpf =11147744" );
+		executa("DELETE FROM Cliente WHERE cpf = 13777555");
+		listar_cliente();
+		desconecta();
 	}
 
+
+public static void conecta() {
+	String driver = "com.mysql.jdbc.Drivers";//Endereço da classe do driver que corresponde ao banco de dados(Baixar - baixar conector jdbc mysql) - Aula 10 - 10:15 - https://www.youtube.com/watch?v=HOsdNP7Jutw 
+	String url = "jdbc:mysql://localhost:0000/TrabPOO-Av2" ;//Padrão - Verificar Porta de localhost - Nome do banco
+	String login = "root";//Verificar login
+	String senha = "";//Verificar senha
+	try {
+		Class.forName(driver);
+		conexao = DriverManager.getConnection(url, login, senha);
+		System.out.println("Conexão Bem Sucedida");
+		
+	} catch (Exception e) {
+		e.printStackTrace();//Nesta linha é impresso o erro!
+	}
+}
+
+
+public static void executa(String sql)throws Exception{//OBS : Metodo executa está sendo repetido nas duas classes !!! 
+	Statement st = null;
+	st = conexao.createStatement();
+	st.executeUpdate(sql);
+	}
+
+public static void listar_cliente() throws Exception{
+	Statement st;
+	st = conexao.createStatement();
+	ResultSet rs = st.executeQuery("SELECT cpf, nome, telefone FROM Cliente");
+	System.out.println("----------------------------------------");
+	while (rs.next()) {
+		System.out.println("CPF:" +rs.getString(1) + "Nome: "+ rs.getString(2)+ "Telefone: " + rs.getString(3) );
+		}
+	}
+public static void desconecta() throws Exception{
+	conexao.close();
+	}
 }
