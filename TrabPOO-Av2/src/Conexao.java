bc50b66109f9f7089d8ef2815230ed7b73b388bc
rafaelfaustini <contato@rafaelfaustini.com.br>
2019-05-27 11:21:53 -0300

    Revert "Trabalho"
    This reverts commit 018295ae886da70f783ab3900cfbfbb66653dbb7.

--- TrabPOO-Av2/src/Conexao.java
+++ TrabPOO-Av2/src/Conexao.java
@@ -3,11 +3,11 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Conexao {
-String severName = "sql52.main-hosting.eu";//Caminho do servidor do BD
-String mydatabase = "u557001243_poo";//Nome do banco de dados
+String severName = "localhost";//Caminho do servidor do BD
+String mydatabase = "teste";//Nome do banco de dados
 String url = "jdbc:mysql//" + severName + " / " + mydatabase;
-String username = "u557001243_poo";//Nome do usuário de seu BD
-String password = "WIq9j8o0sM4l";//Sua senha de acesso
+String username = "root";//Nome do usuário de seu BD
+String password = "";//Sua senha de acesso
 Connection conexao;
 
 Conexao()throws SQLException{
