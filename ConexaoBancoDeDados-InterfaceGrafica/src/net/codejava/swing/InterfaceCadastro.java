52b0a68c42654d8df8777e742b4a10f47fac4109
Organela <andre.luiz.cunha95@gmail.com>
2019-06-04 18:16:16 -0300

    Interface Cadastro adicionada
    Interface Cadastro adicionada

---
+++ ConexaoBancoDeDados-InterfaceGrafica/src/net/codejava/swing/InterfaceCadastro.java
@@ -0,0 +1,63 @@
+package net.codejava.swing;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.Font;
+
+public class InterfaceCadastro extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField textField;
+	private final JButton btnNewButton = new JButton("Enviar");
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					InterfaceCadastro frame = new InterfaceCadastro();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public InterfaceCadastro() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 475, 127);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+		
+		JLabel lblDeONome = new JLabel("De o nome, cpf e telefone de cliente: ");
+		lblDeONome.setFont(new Font("Verdana", Font.PLAIN, 17));
+		contentPane.add(lblDeONome, BorderLayout.NORTH);
+		
+		textField = new JTextField();
+		contentPane.add(textField, BorderLayout.CENTER);
+		textField.setColumns(10);
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		contentPane.add(btnNewButton, BorderLayout.SOUTH);
+	}
+
+}
