660f02bf4626a5b87d913f1324fd6665cd633549
Organela <andre.luiz.cunha95@gmail.com>
2019-06-04 15:31:08 -0300

    Classes no mesmo pacote

---
+++ ConexaoBancoDeDados-InterfaceGrafica/src/net/codejava/swing/Cliente.java
@@ -0,0 +1,47 @@
+package net.codejava.swing;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Cliente {
+	
+	private String nome;
+	private int cpf;
+	private int telefone;
+	
+	public Cliente(String nome, int cpf, int telefone) throws SQLException {
+		
+		this.nome = nome;
+		this.cpf = cpf;
+		this.telefone = telefone;
+		Conexao con = new Conexao();
+		String query = "INSERT INTO Cliente(nome, cpf, telefone) VALUES ('" + this.getNome() + "','" + this.getCpf() + "','" + this.getTelefone() + "')";
+		Statement st = con.conexao.createStatement();
+		st.executeUpdate(query);
+ 
+	}
+
+	public void Status(Cliente a) {
+		System.out.println("Nome: " + a.getNome() +" Cpf: "+ a.getCpf() + " Telefone: " + a.getTelefone());
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public int getCpf() {
+		return cpf;
+	}
+	public void setCpf(int cpf) {
+		this.cpf = cpf;
+	}
+	public int getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(int telefone) {
+		this.telefone = telefone;
+	}
+	
+	
+}
+
