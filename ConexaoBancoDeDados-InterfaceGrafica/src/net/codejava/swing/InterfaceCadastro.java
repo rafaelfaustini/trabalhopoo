7eecc7e15d4814946f451cb28978222b478c0718
DRCorreia <48036778+DRCorreia@users.noreply.github.com>
2019-06-05 00:11:26 -0300

    Update InterfaceCadastro.java

--- ConexaoBancoDeDados-InterfaceGrafica/src/net/codejava/swing/InterfaceCadastro.java
+++ ConexaoBancoDeDados-InterfaceGrafica/src/net/codejava/swing/InterfaceCadastro.java
@@ -6,18 +6,24 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
 import javax.swing.JLabel;
-import javax.swing.JTextField;
-import javax.swing.JButton;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
 import java.awt.Font;
+import javax.swing.JTextField;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.JButton;
+import java.awt.Color;
+import javax.swing.border.CompoundBorder;
+import javax.swing.UIManager;
+import javax.swing.JTable;
 
 public class InterfaceCadastro extends JFrame {
 
 	private JPanel contentPane;
-	private JTextField input;
-	private final JButton btnNewButton = new JButton("Enviar");
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
 
 	/**
 	 * Launch the application.
@@ -39,27 +45,83 @@ public class InterfaceCadastro extends JFrame {
 	 * Create the frame.
 	 */
 	public InterfaceCadastro() {
-		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 475, 127);
+		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
+		contentPane.setBackground(new Color(230, 230, 250));
+		contentPane.setBorder(UIManager.getBorder("Button.border"));
 		setContentPane(contentPane);
 		
-		JLabel lblDeONome = new JLabel("De o nome, cpf e telefone de cliente: ");
-		lblDeONome.setFont(new Font("Verdana", Font.PLAIN, 17));
-		contentPane.add(lblDeONome, BorderLayout.NORTH);
+		JLabel lblCadastroDoCliente = new JLabel("Cadastro do Cliente");
+		lblCadastroDoCliente.setFont(new Font("Arial", Font.BOLD, 28));
 		
-		input = new JTextField();
-		contentPane.add(input, BorderLayout.CENTER);
-		input.setColumns(10);
-		btnNewButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-			}
-		});
-		contentPane.add(btnNewButton, BorderLayout.SOUTH);
-			}
-		}
-
-
+		JLabel lblNome = new JLabel("Nome:");
+		lblNome.setBackground(new Color(169, 169, 169));
+		lblNome.setFont(new Font("Cambria Math", Font.BOLD, 18));
+		
+		textField = new JTextField();
+		textField.setColumns(10);
+		
+		JLabel lblCpf = new JLabel("CPF:");
+		lblCpf.setFont(new Font("Cambria Math", Font.BOLD, 18));
+		
+		textField_1 = new JTextField();
+		textField_1.setColumns(10);
+		
+		JLabel lblTel = new JLabel("TEL:");
+		lblTel.setFont(new Font("Cambria Math", Font.BOLD, 18));
+		
+		textField_2 = new JTextField();
+		textField_2.setColumns(10);
+		
+		JButton btnEnviar = new JButton("ENVIAR");
+		btnEnviar.setBackground(new Color(255, 255, 224));
+		btnEnviar.setForeground(new Color(0, 0, 0));
+		btnEnviar.setFont(new Font("Arial", Font.BOLD, 16));
+		GroupLayout gl_contentPane = new GroupLayout(contentPane);
+		gl_contentPane.setHorizontalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(Alignment.TRAILING, gl_contentPane.createSequentialGroup()
+					.addContainerGap(95, Short.MAX_VALUE)
+					.addComponent(lblCadastroDoCliente)
+					.addGap(78))
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addGap(34)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING)
+						.addComponent(lblNome)
+						.addComponent(lblCpf)
+						.addComponent(lblTel))
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+						.addComponent(textField_2, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(textField_1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(textField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+					.addContainerGap(161, Short.MAX_VALUE))
+				.addGroup(Alignment.TRAILING, gl_contentPane.createSequentialGroup()
+					.addContainerGap(183, Short.MAX_VALUE)
+					.addComponent(btnEnviar)
+					.addGap(162))
+		);
+		gl_contentPane.setVerticalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addComponent(lblCadastroDoCliente)
+					.addGap(30)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+						.addComponent(lblNome)
+						.addComponent(textField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+					.addGap(18)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+						.addComponent(lblCpf)
+						.addComponent(textField_1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+					.addGap(18)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+						.addComponent(lblTel)
+						.addComponent(textField_2, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+					.addGap(34)
+					.addComponent(btnEnviar)
+					.addContainerGap())
+		);
+		contentPane.setLayout(gl_contentPane);
+	}
+}
